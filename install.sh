80c45dda47abd6abfffec89b6803398439c40363
Cristian <cristian@localhost.localdomain>
2021-06-12 10:12:42 +0200

    Added custom config for nvim Added .gitignore Updated install.sh script for installing nvim config

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # vim
 ln -fs ${BASEDIR}/.vimrc ~/.vimrc
-ln -fs ${BASEDIR}/.vimrc ~/.config/nvim/init.vim 
 
 # kitty
-rm -rf ~/.config/kitty 
+rm -rf ~/.config/kitty
 ln -s  ${BASEDIR}/.config/kitty ~/.config/kitty
+
+# nvim
+rm -rf ~/.config/nvim
+ln -s  ${BASEDIR}/.config/nvim ~/.config/nvim
