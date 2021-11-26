c120d9935ac3937474b725aa9c644aa4c6b0b499
Cristian Cancela Lema <cristiancancelalema@gmail.com>
2021-11-26 19:52:07 +0100

    Changed vagrant for support Fedora

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -2,54 +2,48 @@
 
 VAGRANT_USER_PASS=vagrant
 VAGRANT_SHARED_FOLDER=/vagrant
-VAGRANT_PROJECT_FOLDER=${VAGRANT_SHARED_FOLDER}/projects
 VAGRANT_HOME=/home/vagrant
 
-# UBUNTU
+sudo dnf upgrade -y
+sudo dnf group install -y "Fedora Workstation" "i3 desktop"
+sudo systemctl set-default graphical.target
+sudo systemctl enable gdm.service
+sudo dnf install -y git vim neovim zsh kitty wget podman podman-compose skopeo buildah python3-pip python3.8 \
+    poetry nodejs npm golang gnome-tweaks alacarte origin-clients kubernetes-client
 
-# Configure docker repository
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Configure GITHUB client https://github.com/cli/cli"
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install -y gh
 
-sudo apt update
-sudo apt install -y git vim zsh wget docker-ce docker-ce-cli containerd.io gnome gnome-tweak-tool python3-pip python3-venv nodejs npm alacarte
-sudo usermod -a -G docker vagrant
+# Configure spanish keyboard layout for gnome
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'es')]"
 
 # I3 window mannager setup
-sudo apt install -y i3 i3status dmenu rofi i3lock xbacklight feh conky lxappearance arc-theme fonts-font-awesome fonts-powerline
-pip3 install bumblebee-status # Bar mannager for i3
+sudo apt install -y i3status dmenu rofi i3lock xbacklight feh conky lxappearance arc-theme fontawesome-fonts powerline powerline-fonts
+pip install bumblebee-status # Bar mannager for i3
 sudo timedatectl set-timezone Europe/Madrid
 
-# Install kitty terminal
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-sudo ln -s /home/vagrant/.local/kitty.app/bin/kitty /usr/bin/kitty
-
-# Install and configure Neovim with vim-plug (https://github.com/junegunn/vim-plug)
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-sudo mv nvim.appimage /usr/bin/nvim
+# Configure Neovim with vim-plug (https://github.com/junegunn/vim-plug)
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# Docker Compose instalation
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# FEDORA
-# sudo dnf grouplist -v # List groups for desktop environments
-# sudo dnf install -y vim zsh curl wget podman buildah skopeo kitty neovim @workstation-product-environment
-# sudo dnf install -y i3 i3status dmenu i3lock xbacklight feh conky # i3 window mannager https://fedoramagazine.org/getting-started-i3-window-manager/
 
 cd /opt
 
-echo "GOLANG https://golang.org/dl/"
-GOLANG_VERSION=1.15.8
-GOLANG_TAR_URL="https://golang.org/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz"
-sudo wget ${GOLANG_TAR_URL} &> /dev/null
-sudo tar -zxf $(basename $GOLANG_TAR_URL)
-sudo rm $(basename $GOLANG_TAR_URL)
-GOROOT=/opt/$(ls | grep go | head -1)
+#echo "GOLANG https://golang.org/dl/"
+#GOLANG_VERSION=1.15.8
+#GOLANG_TAR_URL="https://golang.org/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz"
+#sudo wget ${GOLANG_TAR_URL} &> /dev/null
+#sudo tar -zxf $(basename $GOLANG_TAR_URL)
+#sudo rm $(basename $GOLANG_TAR_URL)
+#GOROOT=/opt/$(ls | grep go | head -1)
+export GOPATH=${VAGRANT_HOME}/go
+
+echo "KIND https://kind.sigs.k8s.io/"
+GO111MODULE="on" go get -v sigs.k8s.io/kind@v0.11.1
+
+echo "KUBECTL https://kubernetes.io/es/docs/tasks/tools/install-kubectl/"
+sudo curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x ./kubectl
+sudo mv kubectl ${GOPATH}/bin
 
 echo "JDK 11 https://adoptopenjdk.net/"
 JDK_TAR_URL=https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.10%2B9/OpenJDK11U-jdk_x64_linux_hotspot_11.0.10_9.tar.gz
@@ -74,9 +68,6 @@ sudo tar -zxf $(basename $SPARK_TAR_URL)
 sudo rm $(basename $SPARK_TAR_URL)
 SPARK_HOME=/opt/$(ls | grep spark | head -1)
 
-# Configure spanish keyboard layout for gnome
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'es')]"
-
 # Nvm and node install
 export PROFILE=$HOME/.zshrc
 export NODE_VERSION=16.3.0
@@ -91,12 +82,12 @@ echo "$VAGRANT_USER_PASS" | chsh -s $(which zsh)
 
 cat <<-EOFILE > "/vagrant/vagrant_env.sh"
 #!/bin/bash
-export GOROOT=${GOROOT}
-export GOPATH=${VAGRANT_HOME}/go
+#export GOROOT=${GOROOT}
+export GOPATH=${GOPATH}
 export JAVA_HOME=${JAVA_HOME}
 export MAVEN_HOME=${MAVEN_HOME}
 export SPARK_HOME=${SPARK_HOME}
-export PATH=\${GOROOT}/bin:\${GOPATH}/bin:\${JAVA_HOME}/bin:\${MAVEN_HOME}/bin:\${SPARK_HOME}/bin:\${HOME}/.local/bin:\${PATH}
+export PATH=\${GOPATH}/bin:\${JAVA_HOME}/bin:\${MAVEN_HOME}/bin:\${SPARK_HOME}/bin:\${HOME}/.local/bin:\${PATH}
 EOFILE
 chmod +x /vagrant/vagrant_env.sh
 
