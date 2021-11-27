4f5e2c6c31d2edda24681e73907520b329b21812
Cristian Cancela Lema <cristiancancelalema@gmail.com>
2021-11-27 14:42:38 +0100

    Fixed problem with vagrant default shared folder

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -2,13 +2,13 @@
 set -e
 
 VAGRANT_USER_PASS=vagrant
-VAGRANT_SHARED_FOLDER=/vagrant
 VAGRANT_HOME=/home/vagrant
 
 sudo dnf upgrade -y
 sudo dnf group install -y "Fedora Workstation" "i3 desktop"
 sudo systemctl set-default graphical.target
-sudo systemctl enable gdm.service
+#sudo systemctl disable lightdm.service
+#sudo systemctl enable gdm.service
 sudo dnf install -y git vim neovim zsh kitty wget podman podman-compose skopeo buildah python3-pip python3.8 \
     poetry nodejs npm golang gnome-tweaks gnome-extensions-app alacarte origin-clients helm gparted sqlite xset jq
 
@@ -29,7 +29,7 @@ sudo timedatectl set-timezone Europe/Madrid
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Install custom dotfiles
-cd ${HOME} && git clone https://github.com/cristiancl25/dotfiles -b develop
+cd ${HOME} && git clone https://github.com/cristiancl25/dotfiles -b feature/fedora
 ./dotfiles/install.sh
 
 cd /opt
@@ -43,8 +43,8 @@ cd /opt
 #GOROOT=/opt/$(ls | grep go | head -1)
 export GOPATH=${VAGRANT_HOME}/go
 
-echo "KIND https://kind.sigs.k8s.io/"
-GO111MODULE="on" go get -v sigs.k8s.io/kind@v0.11.1
+#echo "KIND https://kind.sigs.k8s.io/"
+#GO111MODULE="on" go get -v sigs.k8s.io/kind@v0.11.1
 
 echo "JDK 11 https://adoptopenjdk.net/"
 JDK_TAR_URL=https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.10%2B9/OpenJDK11U-jdk_x64_linux_hotspot_11.0.10_9.tar.gz
