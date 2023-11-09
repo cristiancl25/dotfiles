6d49d05b4b44cd3fd745cbf973baa9198cfbe99e
Cristian Cancela Lema <cristiancle@ext.inditex.com>
2023-11-09 15:44:39 +0100

    Added vim-fugitive plugin to neovim config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+  "n",
+  "<leader>fg",
+  require("telescope.builtin").git_files,
+  { noremap = true, silent = true, desc = "Find Git Files" }
+)
+
+vim.keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<CR>", { noremap = true, silent = true, desc = "Git diff" })
