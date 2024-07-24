6378b305905dc577dec2f822b7aca2e579baad46
Cristian Cancela Lema <cristiancle@ext.inditex.com>
2024-07-24 14:44:27 +0200

    feat: Updated neovim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,17 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set(
-  "n",
-  "<leader>fg",
-  require("telescope.builtin").git_files,
-  { noremap = true, silent = true, desc = "Find Git Files" }
-)
-
+vim.keymap.set("n", "<leader>fg", require("telescope.builtin").git_files, { noremap = true, silent = true, desc = "Find Git Files" })
 vim.keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<CR>", { noremap = true, silent = true, desc = "Git diff" })
-
-vim.keymap.set('i', '<C-j>', 'copilot#Accept("<CR>")', {
-  expr = true,
-  replace_keycodes = false
-})
+vim.keymap.set("n", "<leader>gn", "<cmd>Neogit<CR>", { noremap = true, silent = true, desc = "Neogit" })
+vim.keymap.set("i", "<C-j>", 'copilot#Accept("<CR>")', { expr = true, replace_keycodes = false, })
 vim.g.copilot_no_tab_map = true
