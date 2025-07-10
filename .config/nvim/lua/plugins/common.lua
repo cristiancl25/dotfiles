9395ec90b1954465dff31e6238dff166fa8798ec
cristiancl25 <18629825+cristiancl25@users.noreply.github.com>
2025-07-10 10:37:12 +0200

    feat: Updated lazyvim config and duckdb version in asdf

--- .config/nvim/lua/plugins/common.lua
+++ .config/nvim/lua/plugins/common.lua
@@ -5,4 +5,18 @@ return {
     dependencies = { "MunifTanjim/nui.nvim" },
     opts = {},
   },
+
+  {
+    "shahshlok/vim-coach.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("vim-coach").setup()
+    end,
+    keys = {
+      { "<leader>?", "<cmd>VimCoach<cr>", desc = "Vim Coach" },
+    },
+  },
 }
