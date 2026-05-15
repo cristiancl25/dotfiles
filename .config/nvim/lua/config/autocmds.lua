5eb07751138e5d7b897cc0a8e69b34c4d5fb0d47
Cristian Cancela Lema <cristian.cancela@atramentum.com>
2026-05-15 20:13:23 +0200

    feat: nvim config claude fix

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -5,9 +5,9 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "sql",
   callback = function()
-    vim.bo.shiftwidth = 4
-    vim.bo.tabstop = 4
-    vim.bo.softtabstop = 4
-    vim.bo.expandtab = true
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
   end,
 })
--- .config/nvim/lua/plugins/yaml.lua
+++ .config/nvim/lua/plugins/yaml.lua
@@ -1,8 +1,27 @@
 return {
-  "cuducos/yaml.nvim",
-  ft = { "yaml" }, -- optional
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-telescope/telescope.nvim", -- optional
+  {
+    "cuducos/yaml.nvim",
+    ft = { "yaml" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers.yamlls = opts.servers.yamlls or {}
+      opts.servers.yamlls.settings = opts.servers.yamlls.settings or {}
+      opts.servers.yamlls.settings.yaml = opts.servers.yamlls.settings.yaml or {}
+      local schemas = opts.servers.yamlls.settings.yaml.schemas or {}
+      schemas["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/dbt_yml_files-latest.json"] = {
+        "**/models/**/*.yml",
+        "**/seeds/**/*.yml",
+        "**/snapshots/**/*.yml",
+      }
+      schemas["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/dbt_project-latest.json"] = "dbt_project.yml"
+      opts.servers.yamlls.settings.yaml.schemas = schemas
+    end,
   },
 }
