2730c5bf4907083d967b811e209d0b6c9b480d12
Cristian Cancela Lema <cristian.cancela@atramentum.com>
2026-05-15 20:15:14 +0200

    feat: Deleted unused dotfiles

--- install.sh
+++ install.sh
@@ -6,16 +6,6 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rm -rf ~/.config/nvim
 ln -s  ${BASEDIR}/.config/nvim ~/.config/nvim
 
-# kitty
-rm -rf ~/.config/kitty
-ln -s  ${BASEDIR}/.config/kitty ~/.config/kitty
-
-# i3 WM
-rm -rf ~/config/i3
-ln -s ${BASEDIR}/.config/i3 ~/.config/i3
-rm -rf ~/.config/rofi
-ln -s ${BASEDIR}/.config/rofi ~/.config/rofi
-
 # zellij
 rm -rf ~/.config/zellij
 ln -s ${BASEDIR}/.config/zellij ~/.config/zellij
